2aa8e8d1d77cf6a6c497ffe71c946e45f832a1b0
Daniel Singer <dannys430@yahoo.com>
2020-06-01 19:56:11 -0400

    user & session routes, application controller methods complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   root to: "static_pages#root"
 
+  resources :users, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+
+  resource :session, only: [:new, :create, :destroy]
+
+  
+
 end
