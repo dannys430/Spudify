63d6feb337babfbe9d5c19dca67fcbf6479f6dcc
Daniel Singer <dannys430@yahoo.com>
2020-06-01 22:10:12 -0400

    added password length validation, added User and Session routes, and started UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+  
+  before_action :ensure_signed_in, only: [:index, ]
+
+  def new
+    @user = User.new
+
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user
+    else
+      render json: @user.errors.full_messages
+    end
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :password, :name, :birth_date, :gender)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
 
   root to: "static_pages#root"
 
-  resources :users, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :users, only: [:new, :create, :index, :show, :edit, :update, :destroy]
 
   resource :session, only: [:new, :create, :destroy]
 
-  
+
 
 end
