73d11ed9d45822ecbb62a98f80ff85fff89ffee6
Daniel Singer <dannys430@yahoo.com>
2020-06-02 15:06:30 -0400

    added new, create to users controller, and new, create, destroy to sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+
+  before_action :ensure_signed_in, only: [:destroy]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+    if @user
+      sign_in(@user)
+      #redirect
+    else
+      flash.now[:errors] = ['Invalid email or password']
+      render :new
+    end
+  end
+
+  def destroy
+    sign_out
+    #redirect
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,18 @@ class UsersController < ApplicationController
 
   def new
     @user = User.new
-
+    render :new
   end
 
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user
+      sign_in(@user)
+      #redirect
     else
-      render json: @user.errors.full_messages
+      flash.now[:errors] = @user.errors.full_messages
+      # render json: @user.errors.full_messages
+      render :new
     end
   end
 
