c7ef1d23fa86dead6aee2b18abac5b40fe4e2c61
Daniel Singer <dannys430@yahoo.com>
2020-06-05 11:28:02 -0400

    playlist migration complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_183632) do
+ActiveRecord::Schema.define(version: 2020_06_05_151245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "playlists", force: :cascade do |t|
+    t.string "playlist_name", null: false
+    t.text "description"
+    t.integer "user_id", null: false
+    t.boolean "private", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["playlist_name"], name: "index_playlists_on_playlist_name"
+    t.index ["user_id"], name: "index_playlists_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
