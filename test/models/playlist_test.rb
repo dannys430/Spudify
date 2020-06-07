74b94cd98a6f7dd8a44f4b64d10f9b9eb5c5e50d
Daniel Singer <dannys430@yahoo.com>
2020-06-07 14:31:36 -0400

    added playlist model validations

--- test/models/playlist_test.rb
+++ test/models/playlist_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: playlists
+#
+#  id            :bigint           not null, primary key
+#  playlist_name :string           not null
+#  description   :text
+#  user_id       :integer          not null
+#  private       :boolean          default(FALSE), not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 require 'test_helper'
 
 class PlaylistTest < ActiveSupport::TestCase
