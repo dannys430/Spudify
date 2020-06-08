4183a461b059f8adf0d4eb3573041499eeef3c42
Daniel Singer <dannys430@yahoo.com>
2020-06-08 15:34:25 -0400

    fixing heroku issues

--- frontend/components/session/splash.jsx
+++ frontend/components/session/splash.jsx
@@ -23,7 +23,12 @@ class Splash extends React.Component {
             </div>
             <div className="section1-div2">
               <ul>
-                <li><Link to=""><img src={window.test1URL} /></Link></li>
+                <li><Link to=""><img src={window.img1URL} /></Link></li>
+                <li><Link to=""><img src={window.img2URL} /></Link></li>
+                <li><Link to=""><img src={window.img3URL} /></Link></li>
+                <li><Link to=""><img src={window.img4URL} /></Link></li>
+                <li><Link to=""><img src={window.img5URL} /></Link></li>
+                <li><Link to=""><img src={window.img6URL} /></Link></li>
               </ul>
               
             </div>
