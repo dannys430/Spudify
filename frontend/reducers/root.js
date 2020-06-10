bc39dbf674f9aa6665ac7ae8f85b83e96351d8e6
Daniel Singer <dannys430@yahoo.com>
2020-06-10 09:54:01 -0400

    updated root reducer

--- frontend/reducers/root.js
+++ frontend/reducers/root.js
@@ -3,11 +3,13 @@ import {combineReducers} from 'redux';
 import EntitiesReducer from './entities_reducer'
 import SessionReducer from './session';
 import ErrorsReducer from './errors';
+import UIReducer from './ui_reducer';
 
 const RootReducer = combineReducers({
   session: SessionReducer,        // state.session will point to return value of session reducer
   errors: ErrorsReducer,          // etc.
-  entities: EntitiesReducer
+  entities: EntitiesReducer,
+  ui: UIReducer,
 })
 
 export default RootReducer;
