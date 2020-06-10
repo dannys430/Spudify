03fc32670aaabf33590808f0c4051429746d1193
Daniel Singer <dannys430@yahoo.com>
2020-06-10 09:53:25 -0400

    updated playlist_reducer.js

--- frontend/reducers/playlist_reducer.js
+++ frontend/reducers/playlist_reducer.js
@@ -1,9 +1,9 @@
-import { RECEIVE_CURRENT_PLAYLIST } from '../actions/playlist_actions'
+import { RECEIVE_PLAYLIST } from '../actions/playlist_actions'
 
 const PlaylistReducer = (state = {}, action) => {
   Object.freeze(state)
   switch (action.type) {
-    case RECEIVE_CURRENT_PLAYLIST:
+    case RECEIVE_PLAYLIST:
       return Object.assign({}, state, {[action.type.id]: action.type});
     default:
       return state;
