3be4771739c47cd0f4f3117c9cae3b872c92506b
Daniel Singer <dannys430@yahoo.com>
2020-06-10 09:14:45 -0400

    added left_nav_container.js

---
+++ frontend/components/leftnav/left_nav_container.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+// import { fetchNewSong } from '../../actions/song_actions';
+
+import { openModal } from '../../actions/modal_actions';
+
+import LeftNav from './left_nav';
+
+
+const mSTP = (state) => {
+  // debugger
+  return {
+    playlists: state.session.entities.PlaylistReducer
+  }
+}
+
+const mDTP = (dispatch) => {
+  return {
+    // fetchNewSong: (id) => dispatch(fetchNewSong(id))
+    otherForm: (
+      <button className="create-playlist" onClick={() => dispatch(openModal('playlist'))}>
+        Create Playlist
+      </button>
+    )
+  }
+}
+
+export default connect(null, mDTP)(LeftNav);
