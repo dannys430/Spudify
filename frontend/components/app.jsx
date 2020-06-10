a30b0cee8d833ff2feeed266a7081c364a245076
Daniel Singer <dannys430@yahoo.com>
2020-06-10 09:33:02 -0400

    updated app.jsx

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 
-import {AuthRoute} from '../utils/route_utils'
+import {AuthRoute} from '../utils/route_utils';
 
-import SplashContainer from './session/splash_container'
-import SignupContainer from './session/signup_container'
-import LoginContainer from './session/login_container'
+import SplashContainer from './session/splash_container';
+import SignupContainer from './session/signup_container';
+import LoginContainer from './session/login_container';
 
-import HomeContainer from  './home/home_container'
+import HomeContainer from  './home/home_container';
+
+import SongFormContainer from  './song/song_form_container';
+
+import ModalContainer from  './modal/modal_container';
 
 const App = () => (
   <div>
+    <ModalContainer/>
     <Route exact path="/us" component={SplashContainer} />
     <AuthRoute path="/signup" component={SignupContainer} />
     <AuthRoute path="/login" component={LoginContainer} />
 
     <Route exact path="/" component={HomeContainer} />
+
+    <Route exact path="/songform" component={SongFormContainer} />
 
   </div>
 )
