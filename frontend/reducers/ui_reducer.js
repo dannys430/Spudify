497bd86ac7fbb4d547cb97441eecb9a66d12d4a2
Daniel Singer <dannys430@yahoo.com>
2020-06-10 09:56:10 -0400

    added ui_reducer.js

---
+++ frontend/reducers/ui_reducer.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+
+import ModalReducer from './modal_reducer';
+
+const UIReducer = combineReducers({
+  modal: ModalReducer
+});
+
+export default UIReducer;
+
+
+
