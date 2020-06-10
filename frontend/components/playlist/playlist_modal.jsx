fb7fca1b04914b040deeb9378f74d5c0963d1982
Daniel Singer <dannys430@yahoo.com>
2020-06-10 09:24:32 -0400

    updated playlist_modal.jsx

--- frontend/components/playlist/playlist_modal.jsx
+++ frontend/components/playlist/playlist_modal.jsx
@@ -6,8 +6,7 @@ class PlaylistModal extends React.Component {
     super(props)
 
     this.state = {
-      playlist_name: "",
-      user_id: ""
+      playlist_name: ""
     }
 
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -21,42 +20,31 @@ class PlaylistModal extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault()
-    this.props.createNewPlaylist(this.state)
-    .then(() => this.props.history.push(`/`))
+    this.props.createPlaylist(this.state)
+      .then(() => this.props.closeModal())
   }
   
   render() {
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
+      <div className="playlist-modal">
+        <form className="playlist-modal-form" onSubmit={this.handleSubmit}>
           <h1>Create new playlist</h1>
 
-          <label>Playlist Name
-            <input 
-              type="text" 
-              placeholder="New Playlist"
-              onChange={this.update('playlist_name')}
-            />
-          </label>
+          <div className="playlist-modal-input">
+            <h2>Playlist Name</h2>
+              <input
+                type="text"
+                placeholder="New Playlist"
+                onChange={this.update('playlist_name')}
+              />
+          </div>
 
-          {/* <label>Description
-            <input
-              type="text"
-              placeholder="Description"
-              onChange={this.update('description')}
-            />
-          </label> */}
 
-          <label>User Id
-            <input
-              type="text"
-              placeholder="User Id"
-              onChange={this.update('user_id')}
-            />
-          </label>
+          <div className="playlist-modal-buttons-div">
+            <button className="cancel-button" onClick={() => this.props.closeModal()}>CANCEL</button>
+            <button className="create-button" onClick={this.handleSubmit}>CREATE</button>
+          </div>
 
-          <button onClick={this.handleSubmit}>CREATE</button>
-          
         </form>
       </div>
     )
