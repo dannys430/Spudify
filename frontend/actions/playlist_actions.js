12ac2b51a7d1889d2000eb1d6ada1e03c021460f
Daniel Singer <dannys430@yahoo.com>
2020-06-10 09:10:09 -0400

    updated playlist_actions.js

--- frontend/actions/playlist_actions.js
+++ frontend/actions/playlist_actions.js
@@ -1,15 +1,15 @@
-import { createPlaylist } from '../utils/playlist_utils'
+import { createPlaylistRequest } from '../utils/playlist_utils'
 
-export const RECEIVE_CURRENT_PLAYLIST = 'RECEIVE_CURRENT_PLAYLIST'
+export const RECEIVE_PLAYLIST = 'RECEIVE_PLAYLIST'
 
-const receiveCurrentPlaylist = (playlist) => ({
-  type: RECEIVE_CURRENT_PLAYLIST,
+const receivePlaylist = (playlist) => ({
+  type: RECEIVE_PLAYLIST,
   playlist
 })
 
-export const createNewPlaylist = (playlist) => dispatch => {
-  return createPlaylist(playlist)
+export const createPlaylist = (playlist) => dispatch => {
+  return createPlaylistRequest(playlist)
     .then(
-      playlist => dispatch(receiveCurrentPlaylist(playlist))
+      playlist => dispatch(receivePlaylist(playlist))
     )
 }
