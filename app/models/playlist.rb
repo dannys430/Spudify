5864c4d653e519ded3a2ca95b3d8c34ffad128d7
Daniel Singer <dannys430@yahoo.com>
2020-06-11 22:27:21 -0400

    updated playlist.rb

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -17,5 +17,13 @@ class Playlist < ApplicationRecord
   belongs_to :user,
   foreign_key: :user_id,
   class_name: :User
+
+  has_many :playlist_songs,
+  foreign_key: :playlist_id,
+  class_name: :PlaylistSong
+
+  has_many :songs,
+  through: :playlist_songs,
+  source: :song
   
 end
