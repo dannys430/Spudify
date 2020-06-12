10eebbc2cf7169d9620b0c5930d6def9be7774d0
Daniel Singer <dannys430@yahoo.com>
2020-06-11 22:33:55 -0400

    updated playlist_actions.js

--- frontend/actions/playlist_actions.js
+++ frontend/actions/playlist_actions.js
@@ -1,11 +1,23 @@
-import { createPlaylistRequest } from '../utils/playlist_utils'
+import { createPlaylistRequest, fetchPlaylist, fetchPlaylists, fetchPlaylistSongs } from '../utils/playlist_utils'
 
 export const RECEIVE_PLAYLIST = 'RECEIVE_PLAYLIST'
+export const RECEIVE_PLAYLISTS = 'RECEIVE_PLAYLISTS'
+// export const RECEIVE_PLAYLIST_SONGS = 'RECEIVE_PLAYLIST_SONGS'
 
 const receivePlaylist = (playlist) => ({
   type: RECEIVE_PLAYLIST,
   playlist
 })
+
+const receivePlaylists = (playlists) => ({
+  type: RECEIVE_PLAYLISTS,
+  playlists
+})
+
+// const receivePlaylistSongs = (playlistSongs) => ({
+//   type: RECEIVE_PLAYLIST_SONGS,
+//   playlistSongs
+// })
 
 export const createPlaylist = (playlist) => dispatch => {
   return createPlaylistRequest(playlist)
@@ -13,3 +25,24 @@ export const createPlaylist = (playlist) => dispatch => {
       playlist => dispatch(receivePlaylist(playlist))
     )
 }
+
+export const requestPlaylist = (id) => dispatch => {
+  return fetchPlaylist(id)
+    .then(
+      playlist => dispatch(receivePlaylist(playlist))
+    )
+}
+
+export const requestPlaylists = () => dispatch => {
+  return fetchPlaylists()
+    .then(
+      playlists => dispatch(receivePlaylists(playlists))
+    )
+}
+
+// export const requestPlaylistSongs = (id) => dispatch => {
+//   return fetchPlaylistSongs(id)
+//     .then(
+//       playlistSongs => dispatch(receivePlaylistSongs(playlistSongs))
+//     )
+// }
