ad8c7bda1b0c473aeffff6d4a951daa98976ebb0
Daniel Singer <dannys430@yahoo.com>
2020-06-11 22:41:37 -0400

    updated media_bar_container.js

--- frontend/components/mediabar/media_bar_container.js
+++ frontend/components/mediabar/media_bar_container.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import { fetchNewSong } from '../../actions/song_actions';
 
 import MediaBar from './media_bar';
