ee70eea9895c6e49326488b84bedee4e8f51cee5
Daniel Singer <dannys430@yahoo.com>
2020-06-11 22:57:55 -0400

    updated playlist_utils.js

--- frontend/utils/playlist_utils.js
+++ frontend/utils/playlist_utils.js
@@ -5,3 +5,27 @@ export const createPlaylistRequest = (playlist) => {
     data: {playlist}
   })
 }
+
+export const fetchPlaylist = (id) => {
+  // debugger
+  return $.ajax({
+    url: `/api/playlists/${id}`,
+    method: 'GET'
+  })
+}
+
+export const fetchPlaylists = () => {
+  // debugger
+  return $.ajax({
+    url: `/api/playlists/`,
+    method: 'GET'
+  })
+}
+
+// export const fetchPlaylistSongs = (id) => {
+//   // debugger
+//   return $.ajax({
+//     url: `/api/playlists/${id}/songs/`,
+//     method: 'GET'
+//   })
+// }
