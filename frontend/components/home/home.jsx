4f871e980bced9e022c1081fcb06462202c5f692
Daniel Singer <dannys430@yahoo.com>
2020-06-11 22:37:12 -0400

    updated home.jsx

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -3,12 +3,24 @@ import { Link } from 'react-router-dom';
 
 import LeftNavContainer from '../leftnav/left_nav_container'
 import MediaBarContainer from '../mediabar/media_bar_container';
+import MainContentContainer from '../main_content/main_content_container';
 
 class Home extends React.Component {
+  // constructor(props) {
+  //   super(props)
+
+  // }
+
+  // componentDidMount() {
+  //   return this.props.requestPlaylists()
+  // }
+
   render() {
+    // debugger
     return (
       <div>
         <LeftNavContainer />
+        <MainContentContainer />
         <MediaBarContainer />
       </div>
     )
